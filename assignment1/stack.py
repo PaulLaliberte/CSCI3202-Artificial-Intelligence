e1343ea0a604a43af2d7ec9dde447e81d5e223a2
Paul Laliberte <paullaliberte@rgnt1-34-9-dhcp.int.colorado.edu>
2016-08-27 18:26:15 -0600

    Question 3: 25/25

---
+++ assignment1/stack.py
@@ -0,0 +1,60 @@
+"""Question 3 - Assignment 1
+   Stack implementation in python using a list
+   Python version: 2.x"""
+
+
+class Stack():
+
+    def __init__(self):
+        self.newStack = []
+
+    def __len__(self):
+        return len(self.newStack)
+
+    def isEmpty(self):
+        if len(self.newStack) == 0:
+            return True
+        else:
+            return False
+
+    def push(self, integer):
+        return self.newStack.append(integer)
+
+    def pop(self):
+        if self.isEmpty() == True:
+            return "Stack empty."
+        else:
+            return self.newStack.pop()
+
+    def checkSize(self):
+        return len(self.newStack)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Tests needed to pass:
+
+myStack = Stack()
+
+myStack.push(5)
+
+myStack.push(2)
+
+myStack.pop()
+
+myStack.checkSize()
+
+When the stack is initialized, it should be empty.
+"""
