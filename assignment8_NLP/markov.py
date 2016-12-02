766c30f31b7977b4afb49b3ba569bab7c4014185
Paul Laliberte <pala5552@colorado.edu>
2016-12-01 20:06:19 -0700

    calculated transition probs

--- assignment8_NLP/markov.py
+++ assignment8_NLP/markov.py
@@ -1,9 +1,8 @@
-"""Parse tag (txt) files and add
-   <s> (special start tag)
-   </s> (special end tag).
-"""
+"""Using Markov Process, create transition and emission probabilities"""
 
+from __future__ import division
 import re
+
 
 class markovChain():
 
@@ -11,10 +10,10 @@ class markovChain():
         self.processed_list = []
         self.transition_count = {}
         self.total_count = {}
+        self.transition_prob_dict = {}
 
 
     def preProcess(self):
-
         with open("penntree.tag") as f:
             self.processed_list.append("SSSS")
             for line in f:
@@ -25,7 +24,6 @@ class markovChain():
                     self.processed_list.append(line.rstrip())
 
     def transition_probabilities(self):
-        
         list_of_tags = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ',
                         'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP',
                         'NNPS', 'PDT', 'POS', 'PRP', 'PRP$', 'RB', 'RBR',
@@ -33,7 +31,6 @@ class markovChain():
                         'VBG', 'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$',
                         'WRB']
         
-
         #list_of_tags = ['DT', 'NN', 'CC'] #Check. See if DT == 870.
 
         #calculate total number of tags
@@ -52,21 +49,19 @@ class markovChain():
             self.total_count[i] = count
 
 
-        #transmission counts P(Tag_i | Tag_j) = 
-        #(all instance where Tag_i appears DIRECTLY after Tag_j) / (total count of Tagj)
         #we create a dictionary of dictionaries to hold our
         #transition counts
 
 
         for i in list_of_tags:
             self.transition_count[i] = {}
+            self.transition_prob_dict[i] = {}
 
         for i in list_of_tags:
             for j in list_of_tags:
                 if j != i:
                     x = {j : 0}
                     self.transition_count[i].update(x)
-
 
         for i in list_of_tags:
             for j in range(0, len(self.processed_list)):
@@ -91,9 +86,19 @@ class markovChain():
                 except IndexError:
                     #start or end of line
                     pass
-        
+
+        #calculate the probabilities
+        #transmission counts P(Tag_i | Tag_j) = 
+        #(all instance where Tag_i appears DIRECTLY after Tag_j) / (total count of Tagj)
+
+        for i in list_of_tags:
+            for j in list_of_tags:
+                if j != i:
+                    self.transition_prob_dict[i] = self.transition_count[i][j] / self.total_count[i]
 
 
 
+    def emission_probabilities(self):
+        pass
 
 
